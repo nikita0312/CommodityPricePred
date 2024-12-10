88ee178080a3e8665c5d1dfded3a788e3d598f4f
amannaik247 <amancodes247@gmail.com>
2024-12-10 13:49:05 +0530

    modified:   commodai/src/commodai/main.py

--- commodai/src/commodai/main.py
+++ commodai/src/commodai/main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+__import__("pysqlite3")
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 import warnings
 import streamlit as st
 from commodai.src.commodai.crew import Commodai
